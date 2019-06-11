b54f392ff4cbde8e9c1e1c085e015c6044cbf396
rummolprod999 <rummolprod999@gmail.com>
2019-06-11 10:16:31 +0300

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    closure_vec();
+}
+
+
+fn closure_vec(){
+    let haystack = vec![1, 2, 3];
+    //func_vec(haystack);
+    let contains =  |needle| haystack.contains(needle);
+    //func_vec(haystack);
+
+    println!("{:?}", contains(&1));
+    //println!("{}", contains(&4));
+    //println!("There're {} elements in vec", haystack.len());
+}
+
+fn func_vec(v: Vec<i32>) -> bool{
+    v.contains(&1)
 }
